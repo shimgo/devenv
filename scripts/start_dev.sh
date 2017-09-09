643c34ffbb308e0d16cb782fde99944bd36392cc
shimgo <fen.mex.cyc@gmail.com>
2017-09-09 15:17:43 +0900

    [Modify]開発環境構築をVagrant+VirtualBox+Ansibleへ移行するため不要ファイルを削除
    開発環境構築をVagrant+VirtualBox+Ansibleに移行するにあたり不要となるDockerfile等の
    ファイルを削除した。
    
    開発環境もコンテナ内に構築すると、ホストマシンとのファイル共有において
    意識する点が増えたり(コンテナを再起動したらファイルが消えないか等)、パッケージ
    追加や設定変更によるビルドのやり直しが増えたり、あまり使いやすくなかった。
    
    そこで開発環境構築はvagrant upだけで済むような方針をとることにした。
    これにより開発環境OSのバージョンやDockerのバージョン管理が容易になり、環境構築
    で気にするのがVagrantとVirtualBoxだけ、という状態になることを期待する。
    (Ansibleはvagrantのansible_localプロビジョナを利用してホストマシンには
    インストールしないで済むようにする)

--- scripts/start_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-dev_home="$HOME/devenv"
-prj_name='one_progress'
-prj_docker=$HOME'/devenv/home/projects/one-progress/docker'
-docker-compose -p "$prj_name" -f $prj_docker/docker-compose.yml -f $prj_docker/docker-compose-dev.yml up -d
-docker-compose -p "$prj_name" -f $dev_home/docker-compose-devenv.yml up -d
-
--- scripts/stop_dev.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-dev_home="$HOME/devenv"
-prj_name='one_progress'
-prj_docker="$HOME/devenv/home/projects/one-progress/docker"
-docker-compose -p "$prj_name" -f $prj_docker/docker-compose.yml -f $prj_docker/docker-compose-dev.yml down
-docker-compose -p "$prj_name" -f $dev_home/docker-compose-devenv.yml down
-
